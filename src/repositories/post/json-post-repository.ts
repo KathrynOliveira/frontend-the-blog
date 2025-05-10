87e247544123ad7101f1954a40b880b35b4f83d7
Kathryn Oliveira <k.olliver18@gmail.com>
2025-05-10 16:38:33 -0300

    aula 108 json repository

---
+++ src/repositories/post/json-post-repository.ts
@@ -0,0 +1,17 @@
+import { PostModel } from "@/models/post/post-model";
+import { PostRepository } from "./post-repository";
+
+const ROOT_DIR = process.cwd();
+
+export class JsonPostRepository implements PostRepository{
+    private async readFromDisk(){}
+    async findAll(): Promise<PostModel[]> {
+        
+    }
+
+
+}
+
+export class postRepository = new JsonPostRepository();
+
+    console.log(ROOT_DIR)
