587c72bcf76a87f5247d5fab587719a6757f5109
Kathryn Oliveira <k.olliver18@gmail.com>
2025-05-14 10:45:29 -0300

    aula 114 - component container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
+import { Container } from "@/components/Container";
 import { Loader } from "@/components/Loader";
 import PostList from "@/components/PostList";
 import { Suspense } from "react";
 
 export default async function Home() {
   return (
-    <div className="text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100">
-      <header>
-        <h1 className="text-6xl font-bold text-center py-8">Header</h1>
-      </header>
+      <Container>
+        <header>
+            <h1 className="text-6xl font-bold text-center py-8">Header</h1>
+        </header>
 
-      <Suspense fallback={<Loader/>}>
-          <PostList/>
-      </Suspense>
+        <Suspense fallback={<Loader/>}>
+            <PostList/>
+        </Suspense>
 
-      <footer>
-        <h1 className="text-6xl font-bold text-center py-8">Footer</h1>
-      </footer>
-    </div>
+        <footer>
+          <h1 className="text-6xl font-bold text-center py-8">Footer</h1>
+        </footer>
+      </Container>
   );
 }
