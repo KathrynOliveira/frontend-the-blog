f6f1ed8aec29a9cd127bcd4d0003cb215987014b
Kathryn Oliveira <k.olliver18@gmail.com>
2025-05-14 10:57:15 -0300

    aula 115 - component header com breakpoints do tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 import { Loader } from "@/components/Loader";
 import PostList from "@/components/PostList";
 import { Suspense } from "react";
 
 export default async function Home() {
   return (
-      <Container>
-        <header>
+    <Container>
+      <Header/>
+        {/* <header>
             <h1 className="text-6xl font-bold text-center py-8">Header</h1>
-        </header>
+        </header> */}
 
         <Suspense fallback={<Loader/>}>
             <PostList/>
