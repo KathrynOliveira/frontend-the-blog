8ce032242bec941281b25ec7133bcd2e823d0301
Kathryn Oliveira <k.olliver18@gmail.com>
2025-05-14 10:29:26 -0300

    aula 113 - theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,17 @@ import { Suspense } from "react";
 
 export default async function Home() {
   return (
-    <div>
-      <header>Header</header>
+    <div className="text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100">
+      <header>
+        <h1 className="text-6xl font-bold text-center py-8">Header</h1>
+      </header>
 
       <Suspense fallback={<Loader/>}>
           <PostList/>
       </Suspense>
 
       <footer>
-          Footer
+        <h1 className="text-6xl font-bold text-center py-8">Footer</h1>
       </footer>
     </div>
   );
