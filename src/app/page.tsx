a9cd897391740570a346bec623b04d458d27115b
Kathryn Oliveira <k.olliver18@gmail.com>
2025-05-16 17:46:07 -0300

    aula 120 - criando grid responsiva postList com taillwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,7 @@ import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
 import { Loader } from "@/components/Loader";
 import { PostCoverImage } from "@/components/PostCoverImage";
-import { PostHeading } from "@/components/PostHeading";
 import PostList from "@/components/PostList";
-import Image from "next/image";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function Home() {
@@ -13,10 +10,6 @@ export default async function Home() {
     <Container>
       <Header />
       <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
-          <Image className="w-full h-full object-cover object-center group-hover:scale-105 transition"
-            src='/images/bryen_0.png' width={1200} height={720} alt="Titulo do post" priority></Image>
-        </Link>
         <PostCoverImage
           linkProps={{
             href: '/post/asdfasdf',
--- src/components/PostHeading/index.tsx
+++ src/components/PostHeading/index.tsx
@@ -6,10 +6,15 @@ type PostHeadingProps = {
     as?: "h1" | "h2";
 }
 
+const headingClassesMap = {
+  h1: "text-2xl/tight sm:text-4xl font-extrabold",
+  h2: "text-2xl/tight font-bold",
+};
+
 export function PostHeading({ children, url, as: Tag = "h2" }: PostHeadingProps) {
 return (
-    <Tag className="text-2xl/tight font-extrabold sm:text-xl">
-        <Link href={url}>{children}</Link>
-    </Tag>
+  <Tag className={headingClassesMap[Tag]}>
+    <Link className="hover:text-slate-600 transition" href={url}>{children}</Link>
+  </Tag>
 );
 }
