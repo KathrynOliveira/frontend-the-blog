4c8076bbc7771d85cb9d69fb79953111156ba171
Kathryn Oliveira <k.olliver18@gmail.com>
2025-05-23 13:37:26 -0300

    aula 129 - rotas dinamicas

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -5,11 +5,12 @@ import { findAllPublicPosts } from "@/lib/post/queries";
 export async function PostFeatured() {
   const posts = await findAllPublicPosts();
   const post = posts[0];
+  const postLink = `/post/${post.slug}`;
   return (
     <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
       <PostCoverImage
         linkProps={{
-          href: post.slug,
+          href: postLink,
         }}
         imageProps={{
           width: 1200,
@@ -20,7 +21,7 @@ export async function PostFeatured() {
         }}
       />
       <PostSummary
-        postLink={post.slug}
+        postLink={postLink}
         postHeading="h1"
         createdAt={post.createdAt}
         excerpt={post.excerpt}
