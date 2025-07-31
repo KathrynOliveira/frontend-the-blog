39a5b494409f0372a072df0e1dc20ddfc2c7efff
Kathryn Oliveira <k.olliver18@gmail.com>
2025-07-31 15:54:27 -0300

    aula 138 - Component error message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,12 @@
+import ErrorMessage from "@/components/ErrorMessage"
+
 export default function NotFoundPage() {
   return (
-    <>
-      <title>Página não encontrada</title>
-      <div
-        className={
-          "min-h-[320px] bg-slate-900 dark:bg-slate-600 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center"
-        }
-      >
-        <div>
-          <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-          <p>
-            Erro 404 - A página que você está tentando acessar não existe neste
-            site.
-          </p>
-        </div>
-      </div>
-    </>
+      <ErrorMessage
+        pageTitle="Página não encontrada"
+        contentTitle="404"
+        content="Erro 404 - A página que você está tentando acessar não existe neste
+            site."
+      />
   );
 }
