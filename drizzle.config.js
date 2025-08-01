2180021ad5422792c4a3b8a02b068b5497db02c2
Kathryn Oliveira <k.olliver18@gmail.com>
2025-08-01 12:39:16 -0300

    aula 147 - conexão drizzle ORM + migrations

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-    out: './blog-with-udemy/src/db/drizzle/migrations',
-    out: './blog-with-udemy/src/db/drizzle/schemas.ts',
+    out: './src/db/drizzle/migrations',
+    schema: './src/db/drizzle/schemas.ts',
     dialect: 'sqlite',
     dbCredentials: {
         url: './db.sqlite3',
