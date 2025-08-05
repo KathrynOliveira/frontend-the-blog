29f170f59553b57f8348b7c9ddec9d338e5bbd00
Kathryn Oliveira <k.olliver18@gmail.com>
2025-08-05 10:47:46 -0300

    aula 161 - usando unstable_cacche

--- src/db/drizzle/test.ts
+++ src/db/drizzle/test.ts
@@ -1,13 +1,13 @@
-import { drizzleDb } from ".";
-import { postsTable } from "./schemas";
-import { eq } from "drizzle-orm";
+// import { drizzleDb } from ".";
+// import { postsTable } from "./schemas";
+// import { eq } from "drizzle-orm";
 
-(async () => {
-  await drizzleDb
-    .update(postsTable)
-    .set({
-      title: "Rotina matinal de pessoas altamente eficazes",
-      published: true,
-    })
-    .where(eq(postsTable.slug, "rotina-matinal-de-pessoas-altamente-eficazes"));
-})();
+// (async () => {
+//   await drizzleDb
+//     .update(postsTable)
+//     .set({
+//       title: "Rotina matinal de pessoas altamente eficazes",
+//       published: true,
+//     })
+//     .where(eq(postsTable.slug, "rotina-matinal-de-pessoas-altamente-eficazes"));
+// })();
