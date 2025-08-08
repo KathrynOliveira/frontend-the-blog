c919fcdfb43fdd083d60ad201908303cc68d6957
Kathryn Oliveira <k.olliver18@gmail.com>
2025-08-08 12:00:43 -0300

    aula 169 - dialog adicionando funcionalidades

--- src/components/Admin/DeletePostButton/index.tsx
+++ src/components/Admin/DeletePostButton/index.tsx
@@ -1,33 +1,51 @@
-'use client';
+"use client";
 
 import { deletePostAction } from "@/actions/post/delete-post-actions";
+import { Dialog } from "@/components/Dialog";
 import { Trash2Icon } from "lucide-react";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
 
 type DeletePostButtonProps = {
-    id: string;
-    title: string;
+  id: string;
+  title: string;
 };
 export function DeletePostButton({ id, title }: DeletePostButtonProps) {
-    const [isPending, startTransition] = useTransition();
+  const [isPending, startTransition] = useTransition();
+  const [showDialog, setShowDialog] = useState(false);
 
-    function handleClick() {
-        if(!confirm('Tem certeza?')) return;
-        startTransition(async () => {
-            const result = await deletePostAction(id);
-            alert(`O result é: ${result}`);
-        });
-    }
-    return (
+  function handleClick() {
+    setShowDialog(true);
+  }
+
+  function handleConfirm() {
+    startTransition(async () => {
+      const result = await deletePostAction(id);
+      alert(`O result é: ${result}`);
+      setShowDialog(false);
+    });
+  }
+  return (
+    <>
       <button
         className="text-red-500 cursor-pointer transition [&_svg]:w-4 [&_svg]:h-4 
                 hover:scale-120 hover:text-red-700 disabled:text-slate-600 disabled:cursor-not-allowed"
         arial-label={`Apagar post: ${title}`}
-            title={`Apagar post: ${title}`}
-            onClick={handleClick}
-            disabled={isPending}
+        title={`Apagar post: ${title}`}
+        onClick={handleClick}
+        disabled={isPending}
       >
         <Trash2Icon />
       </button>
-    );
+      {showDialog && (
+        <Dialog
+          isVisible={showDialog}
+          title="Apagar post?"
+          content={`Tem certeza que deseja apagar o post: ${title}`}
+          onCancel={() => setShowDialog(false)}
+          onConfirm={handleConfirm}
+          disabled={isPending}
+        />
+      )}
+    </>
+  );
 }
--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -1,6 +1,4 @@
-import { deletePostAction } from "@/actions/post/delete-post-actions";
 import { findAllPostAdmin } from "@/lib/post/queries/admin";
-import { Trash2Icon } from "lucide-react";
 import Link from "next/link";
 import { DeletePostButton } from "../Admin/DeletePostButton";
 
@@ -28,35 +26,7 @@ export default async function PostsListAdmin() {
           </div>
         );
       })}
-
-      <div
-        className="fixed inset-0 bg-black/50 
-      backdrop-blur-xs flex items-center justify-center z-50"
-      >
-        <div className="bg-slate-100 p-6 rounded-lg 
-        max-w-2xl mx-6 flex flex-col gap-6 dark:text-black shadow-lg
-        shadow-black/30 text-center"
-        >
-          <h3 className="text-xl font-extrabold">Título do diálogo</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
-            impedit porro dolores facere, molestiae placeat eveniet
-            exercitationem quod quo error perspiciatis sunt eaque quos dolorum
-            sed, neque eum corporis vitae.
-          </p>
-          <div className="flex items-center justify-around">
-            <button
-              className="bg-slate-200 hover:bg-slate-300 transition text-slate-950 flex items-center justify-center py-2 px-4 rounded-lg cursor-pointer"
-              autoFocus
-            >
-              Cancelar
-            </button>
-            <button className="bg-blue-500 hover:bg-blue-600 transition text-blue-50 flex items-center justify-center py-2 px-4 rounded-lg cursor-pointer">
-              Ok
-            </button>
-          </div>
-        </div>
-      </div>
+      
     </div>
   );
 }
