676762fbc9d80302a2b8f2527ce4cd687bf43ba5
Kathryn Oliveira <k.olliver18@gmail.com>
2025-08-11 13:00:19 -0300

    aula 174 - logica abrir/fechar menu

--- src/components/Admin/MenuAdmin/index.tsx
+++ src/components/Admin/MenuAdmin/index.tsx
@@ -1,12 +1,51 @@
-import { FileTextIcon, HouseIcon } from "lucide-react";
+"use client";
+
+import {
+  CircleXIcon,
+  FileTextIcon,
+  HouseIcon,
+  MenuIcon,
+  PlusIcon,
+} from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export function MenuAdmin() {
-  const navClasses = "bg-slate-900 text-slate-100 rounded-lg flex flex-col overflow-hidden h-10 mb-8 sm:flex-row sm:flex-wrap";
-  const linkClasses = "[&>svg]:w-[16px] [&>svg]:h-[16px] px-4 flex transition hover:bg-slate-800 items-center justify-start gap-2 h-10 shrink-0";
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
 
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
+  const navClasses = `bg-slate-900 text-slate-100 rounded-lg flex flex-col mb-8 sm:flex-row sm:flex-wrap sm:overflow-visible sm:h-auto ${
+    !isOpen && "h-10 overflow-hidden"
+  }`;
+  const linkClasses =
+    "[&>svg]:w-[16px] [&>svg]:h-[16px] px-4 flex transition hover:bg-slate-800 items-center justify-start gap-2 h-10 shrink-0 cursor-pointer";
+
+  const openCloseBtnClasses = "text-blue-200 italic sm:hidden";
   return (
     <nav className={navClasses}>
+      <button
+        onClick={() => setIsOpen((s) => !s)}
+        className={[linkClasses, openCloseBtnClasses].join(" ")}
+      >
+        {!isOpen && (
+          <>
+            <MenuIcon />
+            Menu
+          </>
+        )}
+
+        {isOpen && (
+          <>
+            <CircleXIcon />
+            Fechar
+          </>
+        )}
+      </button>
       <a className={linkClasses} href="/" target="_blank">
         <HouseIcon />
         Home
@@ -15,6 +54,11 @@ export function MenuAdmin() {
       <Link className={linkClasses} href="/admin/post">
         <FileTextIcon />
         Posts
+      </Link>
+
+      <Link className={linkClasses} href="/admin/post/new">
+        <PlusIcon />
+        Criar post
       </Link>
     </nav>
   );
