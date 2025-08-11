46d2c1d66342c98aa4e79aa689b954ff4fc04900
Kathryn Oliveira <k.olliver18@gmail.com>
2025-08-11 11:13:44 -0300

    aula 173 - estilizando menuAdmin

--- src/components/Admin/MenuAdmin/index.tsx
+++ src/components/Admin/MenuAdmin/index.tsx
@@ -1,9 +1,21 @@
+import { FileTextIcon, HouseIcon } from "lucide-react";
 import Link from "next/link";
 
 export function MenuAdmin() {
-    return <nav>
-        <a href="/" target="_blank">Home</a>
+  const navClasses = "bg-slate-900 text-slate-100 rounded-lg flex flex-col overflow-hidden h-10 mb-8 sm:flex-row sm:flex-wrap";
+  const linkClasses = "[&>svg]:w-[16px] [&>svg]:h-[16px] px-4 flex transition hover:bg-slate-800 items-center justify-start gap-2 h-10 shrink-0";
 
-        <Link href="/admin/post">Posts</Link>
+  return (
+    <nav className={navClasses}>
+      <a className={linkClasses} href="/" target="_blank">
+        <HouseIcon />
+        Home
+      </a>
+
+      <Link className={linkClasses} href="/admin/post">
+        <FileTextIcon />
+        Posts
+      </Link>
     </nav>
+  );
 }
