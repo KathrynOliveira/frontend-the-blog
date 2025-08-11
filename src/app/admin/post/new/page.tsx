458a896f06fc989fb8538dc4e50b5ae48fe95b80
Kathryn Oliveira <k.olliver18@gmail.com>
2025-08-11 17:52:19 -0300

    aula 178 - component inputText

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,55 +1,12 @@
-import { Button } from "@/components/Button";
-import { BanIcon, BugIcon, CheckIcon } from "lucide-react";
+import { InputText } from "@/components/InputText";
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <>
-      <div className="py-16 flex gap-4 items-center">
-        <Button variant="default" size="sm">
-          <BugIcon />
-          Confirma
-        </Button>
-        <Button variant="ghost" size="md">
-          <BugIcon />
-          Confirma
-        </Button>
-        <Button variant="danger" size="lg">
-          <BugIcon />
-          Confirma
-        </Button>
-      </div>
-
-      <div className="py-16 flex gap-4 flex-wrap items-center">
-        <Button variant="default" size="sm" disabled>
-          <BugIcon />
-          Confirma
-        </Button>
-        <Button variant="ghost" size="md" disabled>
-          <BugIcon />
-          Confirma
-        </Button>
-        <Button variant="danger" size="lg" disabled>
-          <BugIcon />
-          Confirma
-        </Button>
-
-        <Button variant="danger" size="lg" className="w-full">
-          <BugIcon />
-          Confirma
-        </Button>
-
-        <Button variant="ghost" size="lg" className="w-full">
-          <BanIcon />
-          Cancel
-        </Button>
-
-        <Button variant="default" size="lg" className="w-full">
-          <CheckIcon />
-          OK
-        </Button>
-      </div>
-    </>
+    <div className="py-16 flex-col gap-6 items-center">
+      <InputText labelText="Nome" placeholder="Digite seu nome" />
+      <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" defaultValue="Olá Mundo!" disabled/>
+    </div>
   );
 }
