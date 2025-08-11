151f80edc133a7b7073aee2791143a38211609d1
Kathryn Oliveira <k.olliver18@gmail.com>
2025-08-11 16:10:53 -0300

    aula 176 -  typescript para adicionar variant button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,30 @@
 type ButtonProps = {
+  variant?: ButtonVariants;
+  size?: ButtonSizes;
   children?: React.ReactNode;
 } & React.ComponentProps<"button">;
 
-export function Button({...props }: ButtonProps) {
-  return <button {...props}/>;
+type ButtonVariants = "default" | "ghost" | "danger";
+type ButtonSizes = "sm" | "md" | "lg";
+
+export function Button({
+  variant = "default",
+  size = "md",
+  ...props
+}: ButtonProps) {
+  const buttonVariants: Record<ButtonVariants, string> = {
+    default: "bg-blue-600 text-blue-100",
+    ghost: "bg-slate-200 text-slate-900",
+    danger: "bg-red-600 text-red-100",
+  };
+
+  const buttonSizes: Record<ButtonSizes, string> = {
+    sm: "",
+    md: "",
+    lg: "",
+  };
+
+  const buttonClasses = [buttonVariants[variant], buttonSizes[size]].join(" ");
+
+  return <button className={buttonClasses} {...props} />;
 }
