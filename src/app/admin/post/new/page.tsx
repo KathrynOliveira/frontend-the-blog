6bbaec16483da83dbb1021f8e62770fe1482d802
Kathryn Oliveira <k.olliver18@gmail.com>
2025-08-12 09:51:46 -0300

    aula 179 - component input checkbox

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,12 +1,25 @@
+import { Button } from "@/components/Button";
+import { InputCheckbox } from "@/components/InputCheckbox";
 import { InputText } from "@/components/InputText";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
   return (
-    <div className="py-16 flex-col gap-6 items-center">
-      <InputText labelText="Nome" placeholder="Digite seu nome" />
-      <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" defaultValue="Olá Mundo!" disabled/>
-    </div>
+    <form action="" className="mb-16">
+      <div className="flex flex-col gap-6">
+        <InputText labelText="Nome" placeholder="Digite seu nome" />
+        <InputText
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          defaultValue="Olá Mundo!"
+          disabled
+        />
+        <InputCheckbox labelText="Sobrenome" />
+        <div className="mt-4">
+          <Button type="submit" size="md" className="w-full">Enviar</Button>
+        </div>
+      </div>
+    </form>
   );
 }
