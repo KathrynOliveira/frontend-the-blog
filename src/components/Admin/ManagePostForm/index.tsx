e21648e58bce524b94df421cf6ec52fef3b68bf0
Kathryn Oliveira <k.olliver18@gmail.com>
2025-08-14 16:55:31 -0300

    aula 188 - criando form managePostForm

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -8,33 +8,70 @@ import { useState } from "react";
 import { ImageUploader } from "../ImageUploader";
 
 export function ManagePostForm() {
-  const [contentValue, setContetValue] = useState("");
+  const [contentValue, setContentValue] = useState("");
   return (
     <form action="" className="mb-16">
       <div className="flex flex-col gap-6">
-        <InputText labelText="Nome" placeholder="Digite seu nome" />
         <InputText
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-          defaultValue="Olá Mundo!"
-          disabled
+          labelText="ID"
+          name="id"
+          placeholder="ID gerado automaticamente"
+          type="text"
+          defaultValue={""}
+          readOnly
         />
-              <InputCheckbox labelText="Sobrenome" />
-              
-              <ImageUploader />
+        <InputText
+          labelText="Slug"
+          name="slug"
+          placeholder="Slug gerada automaticamente"
+          type="text"
+          defaultValue={""}
+          readOnly
+        />
+        <InputText
+          labelText="Autor"
+          name="author"
+          placeholder="Digite o nome do autor"
+          type="text"
+          defaultValue={""}
+        />
+        <InputText
+          labelText="Título"
+          name="title"
+          placeholder="Digite o título"
+          type="text"
+          defaultValue={""}
+        />
+        <InputText
+          labelText="Excerto"
+          name="excerpt"
+          placeholder="Digite o resumo"
+          type="text"
+          defaultValue={""}
+        />
 
         <MarkdownEditor
           labelText="Conteúdo"
-          disabled={false}
-          textAreaName="content"
           value={contentValue}
-          setValue={setContetValue}
+          setValue={setContentValue}
+          textAreaName="content"
+          disabled={false}
         />
 
+        <ImageUploader />
+
+        <InputText
+          labelText="URL da imagem de capa"
+          name="coverImageUrl"
+          placeholder="Digite a url da imagem"
+          type="text"
+          defaultValue={""}
+        />
+
+        <InputCheckbox labelText="Publicar?" name="published" type="checkbox" />
+
         <div className="mt-4">
-          <Button type="submit" size="md" className="w-full">
-            Enviar
-          </Button>
+          <Button type="submit">Enviar</Button>
         </div>
       </div>
     </form>
